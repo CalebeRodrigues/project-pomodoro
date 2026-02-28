a17f90ad0973a9ed905660203c4af74a48fa7f54
Calebe Rodrigues <calebebrodrigues@gmail.com>
2026-02-28 17:41:15 -0300

    Finalização do componente Heading e tipagem TypeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>Olá Mundo 2</Heading>
+      <Heading>
+        Olá Mundo 2{' '}
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error sapiente
         odit hic iste, perspiciatis, voluptatibus facere ipsa non reprehenderit
