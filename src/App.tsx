cdc3bf876a4c37ae380568c445fbfb5b3b1a54fc
Calebe Rodrigues <calebebrodrigues@gmail.com>
2026-03-01 15:36:02 -0300

    Criando a estrutura HTML e CSS do nosso app para cada seção do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo 2{' '}
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error sapiente
-        odit hic iste, perspiciatis, voluptatibus facere ipsa non reprehenderit
-        modi ipsam, quaerat quod. Eveniet tempore velit ipsa iusto officiis
-        facere?
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>Logo</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>Menu</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>Form</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>Footer</section>
+        </div>
+      </div>
     </>
   );
 }
