41259abbafc08ade7b88fa0f5f88203b12371bc8
Calebe Rodrigues <calebebrodrigues@gmail.com>
2026-03-01 22:56:38 -0300

    Criando o componente de logo com ícone e link

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
+
       <Container>
-        <Heading>Menu</Heading>
+        <Heading>MENU</Heading>
       </Container>
     </>
   );
